260c6af0326c3564e5216af566a747331458312a
Vittal Pai <vittalpai@in.ibm.com>
2020-05-06 16:17:24 +0530

    Add app logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  buttonText = "Feature Button"
+  featureScreen = false
+
+  constructor(renderer: Renderer,  private cd: ChangeDetectorRef){
+    renderer.listenGlobal('document', 'mfpjsloaded', () => {
+      console.log('--> MobileFirst API plugin init complete');
+      this.checkForFeature()
+    });
+  }
+
+  checkForFeature() {
+    LiveUpdateManager.obtainConfiguration({useClientCache: false}, (configuration) => {
+      console.log("Successfully retreived Liveupdate Configuration" + JSON.stringify(configuration))
+      if(configuration.features["festivalShopping"]) {
+        if(configuration.properties["buttonLabel"] != typeof undefined) {
+          this.buttonText = configuration.properties["buttonLabel"]
+        }
+        this.featureScreen = true
+      } else {
+        this.featureScreen = false
+      }
+      this.cd.detectChanges();
+    }, (failure) => {
+      console.log("Failed to retreive Liveupdate Configuration" + JSON.stringify(failure))
+      this.featureScreen = false
+      this.cd.detectChanges();
+    })
+  }
 
 }
---
+++ src/custom.typings.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="../plugins/cordova-plugin-mfp-liveupdate/typings/liveupdate.d.ts" />
